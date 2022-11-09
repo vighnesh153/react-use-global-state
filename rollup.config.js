781428bc112f8462a0191ba351308cc0ef576e82
Vighnesh Raut <vighnesh.raut13@gmail.com>
2022-11-09 12:44:25 +0000

    build(deps): update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import fs from "fs";
-import babel from "@rollup/plugin-babel";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import typescript from '@rollup/plugin-typescript';
+const fs = require("fs");
+const babel = require("@rollup/plugin-babel");
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const terser = require("@rollup/plugin-terser");
+const typescript = require('@rollup/plugin-typescript');
 
 const pkg = JSON.parse(
   fs.readFileSync("./package.json", { encoding: "utf-8" })
 );
 const extensions = [".ts", ".tsx"];
 
-export default {
+module.exports = {
   input: "src/index.ts",
   external: ["react"],
   plugins: [
